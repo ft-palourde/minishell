1fd37524445c089389400d4dfd43eb76c6206f9f
tcoeffet <teddy.coeffet@gmail.com>
2025-05-23 12:10:49 +0200

    pipes are quite working, still a line return issue to fix

--- src/exec/exec_pipe.c
+++ src/exec/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 18:52:53 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/05/21 17:40:34 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/05/23 12:07:03 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,13 @@ int	exec_pipe(t_tree *node, t_ms *ms)
 	(void) ms;
 	if (!pfd || pipe(pfd) == -1)
 		return (-1);
+	if (node->parent && node->parent->token->type == T_PIPE)
+	{
+		node->right->token->out_fd = node->token->out_fd;
+	}
 	node->left->token->out_fd = pfd[1];
 	node->right->token->in_fd = pfd[0];
+	add_fd(pfd[0], ms->pfd);
+	add_fd(pfd[1], ms->pfd);
 	return (0);
 }
