e1232fe56a855404b9e9af58e3e9d4a44315ac36
tcoeffet <teddy.coeffet@gmail.com>
2025-05-23 12:10:49 +0200

    pipes are quite working, still a line return issue to fix

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -6,15 +6,47 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 11:29:27 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/05/15 15:15:08 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/05/23 07:51:58 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	*add_fd(int fd, int *ms_fd)
+{
+	int	i;
+
+	i = 0;
+	if (!ms_fd)
+	{
+		ms_fd = ft_calloc(2, sizeof(int));
+		if (!ms_fd)
+			return (perror("malloc"), NULL);
+	}
+	while (ms_fd[i])
+		i++;
+	i++;
+	ms_fd = ft_realloc(ms_fd, i * sizeof(int));
+	if (!ms_fd)
+		return (perror("malloc"), NULL);
+	ms_fd[i] = fd;
+	return (ms_fd);
+}
+
 void	close_fds(t_ms *ms)
 {
-	(void) ms;
+	int	i;
+
+	i = 0;
+	while (ms->pfd && ms->pfd[i])
+	{
+		close(ms->pfd[i]);
+		i++;
+	}
+	if (ms->file_in)
+		close(ms->file_in);
+	if (ms->file_out)
+		close(ms->file_out);
 }
 
 int	is_absolute(char *str)
