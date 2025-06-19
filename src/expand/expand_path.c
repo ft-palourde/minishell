7e2bdfb4be05edbd8ff270f26cd891e7a13cc24f
CRaph0705 <raphaelcochrane@hotmail.fr>
2025-06-19 10:09:31 +0200

    expand path last modif added : home joined to path

--- src/expand/expand_path.c
+++ src/expand/expand_path.c
@@ -16,11 +16,26 @@ char	*expand_path(char *str, t_ms *ms);
 
 char	*expand_path(char *str, t_ms *ms)
 {
+	char	*home;
 	char	*expanded_path;
 
-	expanded_path = NULL;
-	if (*str != '~')
-		return (free(expanded_path), NULL);
-	expanded_path = var_name_to_value("HOME", ms);
+	if (!str || str[0] != '~')
+		return (NULL);
+
+	home = var_name_to_value("HOME", ms);
+	if (!home)
+		return (NULL);
+
+	if (str[1] == '\0')
+		expanded_path = ft_strdup(home);
+	else if (str[1] == '/')
+		expanded_path = ft_strjoin(home, str + 1);
+	else
+	{
+		free(home);
+		return (NULL);
+	}
+
+	free(home);
 	return (expanded_path);
 }
