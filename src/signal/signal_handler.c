ae78f7186c0ed1928ca8e6a77fafd4d266314481
Raphael Cochrane <rcochran@f1br3s2.clusters.42paris.fr>
2025-06-24 09:58:55 +0200

    wording

--- src/signal/signal_handler.c
+++ src/signal/signal_handler.c
@@ -43,8 +43,8 @@ void	handle_sigint(int sig)
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
-	// rl_redisplay();
-	close(0);
+	rl_redisplay();
+	close(STDIN_FILENO);
 	return ;
 }
 
