dfe615766dea38f9a7666c9c28044a566546121d
Teddy Coeffet <tcoeffet@f2r11s19.clusters.42paris.fr>
2025-06-25 18:29:04 +0200

    hotfixed exec_redir to avoid double display of perror if addfd fails

--- src/exec/exec_redir.c
+++ src/exec/exec_redir.c
@@ -66,9 +66,9 @@ int	exec_redir(t_token *token, t_ms *ms)
 	if (open_failed(path, ms))
 		return (1);
 	if (in != STDIN_FILENO && add_fd(ms->file_in, ms))
-		return (perror("malloc"), 1);
+		return (1);
 	if (out != STDOUT_FILENO && add_fd(out, ms))
-		return (perror("malloc"), 1);
+		return (1);
 	ms->file_in = in;
 	ms->file_out = out;
 	return (0);
