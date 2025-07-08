cdd18dc76d1955d08603ce7a6f005d7f6bbf73b9
Raphael Cochrane <rcochran@f6r5s2.clusters.42paris.fr>
2025-07-08 11:02:03 +0200

    commented functions and norm ok

--- src/lexing/quote.c
+++ src/lexing/quote.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:10:17 by rcochran          #+#    #+#             */
-/*   Updated: 2025/07/07 16:28:50 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/07/08 11:01:26 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 int		handle_quote(char *input, t_token **tokens);
 int		is_quote(char c);
+int		get_quoted_len(char quote, char *str);
 
-/*
-returns 1 if c == '\'' || c == '\"'
-*/
+/** is_quote - Read a char and return if it is a quote char.
+ * @c: The char read compared to simple and double quote.
+ *
+ * Returns: An int as boolean, 1 true, 0 false.
+ */
 int	is_quote(char c)
 {
 	return (c == '\'' || c == '\"');
 }
 
+/** handle_quote - .
+ * @input: .
+ * @tokens:
+ * 
+ * Returns: The len (int) of .
+ */
 int	handle_quote(char *input, t_token **tokens)
 {
 	t_token			*new_token;
@@ -31,14 +40,7 @@ int	handle_quote(char *input, t_token **tokens)
 
 	len = 1;
 	quote = *input;
-	while (input[len])
-	{
-		if (input[len] == quote && !is_escaped(input, len))
-			break ;
-		len++;
-	}
-	if (input[len] == quote)
-		len++;
+	len = get_quoted_len(quote, input);
 	len += extract_word_len(input + len);
 	new_token = malloc(sizeof(t_token));
 	if (!new_token)
@@ -53,4 +55,24 @@ int	handle_quote(char *input, t_token **tokens)
 	return (len);
 }
 
-// int	get_quote_len();
+/** get_quoted_len - Get the len of the str between 2 same and unescaped quotes.
+ * @quote: quote set as limiter.
+ * @input: str to read until limiter.
+ * 
+ * Returns: The len (int) of the part between same quote type.
+ */
+int	get_quoted_len(char quote, char *input)
+{
+	int	quoted_len;
+
+	quoted_len = 1;
+	while (input[quoted_len])
+	{
+		if (input[quoted_len] == quote && !is_escaped(input, quoted_len))
+			break ;
+		quoted_len++;
+	}
+	if (input[quoted_len] == quote)
+		quoted_len++;
+	return (quoted_len);
+}
