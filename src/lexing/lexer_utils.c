9b9ad37aa4e92552512713ac0a0f2d78851593a9
Raphael Cochrane <rcochran@f6r5s2.clusters.42paris.fr>
2025-07-08 14:48:34 +0200

    is_space commented

--- src/lexing/lexer_utils.c
+++ src/lexing/lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:44:34 by rcochran          #+#    #+#             */
-/*   Updated: 2025/05/26 19:07:17 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/07/08 14:47:38 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int				is_space(char c);
 
+/** is_space - Compares a char to whitespaces.
+ * @c: char read.
+ * 
+ * Check if the read char is a white space.
+ * 
+ * Returns: 1 if true, 0 if false.
+ */
 int	is_space(char c)
 {
 	return ((c >= 9 && c <= 13) || (c == 32));
