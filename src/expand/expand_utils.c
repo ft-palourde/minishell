85daf30e1965930a792109476a7596b2052aa927
Raphael Cochrane <rcochran@f6r5s2.clusters.42paris.fr>
2025-07-09 18:10:40 +0200

    norm and comments added in expand_utils.c

--- src/expand/expand_utils.c
+++ src/expand/expand_utils.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 10:14:41 by rcochran          #+#    #+#             */
-/*   Updated: 2025/07/08 19:17:02 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/07/09 18:10:12 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 char	*get_var_value(char *var);
 int		check_quote_type(char c);
 
-/* 
-return the var part after "=" sign
-*/
+/** @brief get_var_value - Get the value of a var in the env.
+ * @param var The string containing the variable name and its value.
+ * 
+ *
+ * @returns the variable value found after the = sign or NULL if error.
+ */
 char	*get_var_value(char *var)
 {
 	int		i;
@@ -32,6 +35,11 @@ char	*get_var_value(char *var)
 	return (value);
 }
 
+/** @brief check_quote_type - check if it is a single, double or no quote.
+ * @param c The char to read.
+ * 
+ * @returns 1 if simple quote, 2 if double quote, 0 otherwise.
+ */
 int	check_quote_type(char c)
 {
 	if (!c)
