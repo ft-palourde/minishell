6c7dca45921167e3b76c194771c5ad08d4a02e1b
Raphael Cochrane <rcochran@f6r5s2.clusters.42paris.fr>
2025-07-09 18:05:17 +0200

    unused code removed, comment added in variable.c and expand_path.c

--- src/expand/expand_path.c
+++ src/expand/expand_path.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 15:33:45 by rcochran          #+#    #+#             */
-/*   Updated: 2025/07/09 12:42:41 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/07/09 17:43:57 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 char	*expand_path(char *str, t_ms *ms, int exp);
 
+/** @brief expand_path - Get the HOME value in env.
+ * 
+ * @param str The string to expand.
+ * @param ms The minishell structure.
+ * @param exp boolean
+ * 
+ * If the boolean is 0 return NULL, else get the HOME var value.
+ * 
+ * @returns HOME var value or NULL.
+ */
 char	*expand_path(char *str, t_ms *ms, int exp)
 {
 	char	*home;
