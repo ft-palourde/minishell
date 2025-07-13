ft-palourde/minishell

commit 6694a9f9c62f3ea0af11562e788a2b1420550d80

fixed an issue if a redir in was called without command it send its redirection to the next cmd after a pipe

src/exec/exec_pipe.c
@@ -27,12 +27,17 @@ int	exec_pipe(t_tree *node, t_ms *ms)
 	pfd = ft_calloc(2, sizeof(int));
 	if (!pfd || pipe(pfd) == -1)
 		return (perror("minishell"), 1);
+	if (add_pfd(pfd, ms) || add_fd(pfd[0], ms) || add_fd(pfd[1], ms))
+		return (close(pfd[0]), close(pfd[1]), free(pfd), 1);
 	if (node->parent && node->parent->token->type == T_PIPE)
 		node->right->token->out_fd = node->token->out_fd;
-	node->left->token->out_fd = pfd[1];
-	node->right->token->in_fd = pfd[0];
-	add_pfd(pfd, ms);
-	add_fd(pfd[0], ms);
-	add_fd(pfd[1], ms);
+	if (node->left->token->type == T_CMD)
+		node->left->token->out_fd = pfd[1];
+	else
+		close(pfd[1]);
+	if (node->right->token->type == T_CMD)
+		node->right->token->in_fd = pfd[0];
+	else
+		close(pfd[0]);
 	return (0);
 }

src/exec/exec_redir.c
@@ -52,6 +52,17 @@ void	set_ms_fd(int in, int out, t_ms *ms)
 		ms->file_out = out;
 }
 
+int	rd_has_cmd(t_tree *node)
+{
+	while (node->parent && node->parent->token->type != T_PIPE)
+	{
+		if (node->parent->token->type == T_CMD)
+			return (1);
+		node = node->parent;
+	}
+	return (0);
+}
+
 /** exec_redir - execute a redir node
  * @node: current tree node carrying a redir type token
  * @ms: minishell struct
@@ -62,7 +73,7 @@ void	set_ms_fd(int in, int out, t_ms *ms)
  *
  * Returns: 1 on malloc failed, 0 else
  */
-int	exec_redir(t_token *token, t_ms *ms)
+int	exec_redir(t_tree *node, t_ms *ms)
 {
 	char	*path;
 	int		out;
@@ -72,14 +83,16 @@ int	exec_redir(t_token *token, t_ms *ms)
 	out = STDOUT_FILENO;
 	if (ms->open_failed)
 		return (1);
-	path = str_expand(token->data->rd->file->filename, ms);
+	if (!rd_has_cmd(node))
+		return (0);
+	path = str_expand(node->token->data->rd->file->filename, ms);
 	if (!path)
 		return (1);
-	if (token->type == T_REDIR_IN)
+	if (node->token->type == T_REDIR_IN)
 		in = open(path, O_RDONLY, 0644);
-	else if (token->type == T_APPEND)
+	else if (node->token->type == T_APPEND)
 		out = open(path, O_WRONLY | O_APPEND | O_CREAT, 0644);
-	else if (token->type == T_REDIR_OUT)
+	else if (node->token->type == T_REDIR_OUT)
 		out = open(path, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	set_ms_fd(in, out, ms);
 	if (open_failed(path, ms) || \

src/exec/exec_tree.c
@@ -49,7 +49,7 @@ int	exec_tree(t_tree *root, t_ms *ms)
 	if (root->token->type == T_PIPE)
 		exec_pipe(root, ms);
 	if (is_redir(root->token->type))
-		exec_redir(root->token, ms);
+		exec_redir(root, ms);
 	if (root->token->type == T_HEREDOC)
 		ms->file_in = root->token->data->rd->heredoc->fd[0];
 	exec_tree(root->left, ms);

includes/minishell_proto.h
@@ -146,7 +146,7 @@ void			exec_cmd(t_tree *node, t_ms *ms);
 int				exec_tree(t_tree *root, t_ms *ms);
 int				exec_init(t_ms *ms);
 int				exec_pipe(t_tree *node, t_ms *ms);
-int				exec_redir(t_token *token, t_ms *ms);
+int				exec_redir(t_tree *node, t_ms *ms);
 int				get_heredocs_pfd(t_ms *ms);
 void			fill_new_hd(t_ms *ms, int *fd, char *lim, int expand);
 int				check_lim(char	*lim);

commit 3c897a429f1dd3463e1527a74c235afafc9ff5f7

fixed pipe behaviour which was broken since commit 6694a9f and didnt pipe more than once, also fixed a few error codes and syntax of error messages, and finely an error when redirection failed which made the next cmd calling a fd to wait undefinitely

src/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:54:06 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/06/22 18:38:08 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:27:40 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ int	bi_env(char **env)
 	while (env[i])
 	{
 		if (is_set(env[i]))
-			printf("%s\n", env[i]);
+		ft_putendl_fd(env[i], STDOUT_FILENO);
+			// printf("%s\n", env[i]);
 		i++;
 	}
 	return (0);

src/exec/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 18:52:50 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/07/13 15:06:29 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 17:22:03 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,20 +93,20 @@ int	exec_builtin(t_token *token, t_ms *ms)
  */
 unsigned char	command_failed(t_token *token)
 {
-	unsigned char	retval;
+//	unsigned char	retval;
 	struct stat		stt;
 
 	stat(token->data->cmd->path, &stt);
 	if (is_builtin(token))
 		return (0);
-	retval = 126;
+	//retval = 126;
 	ft_putstr_fd("Minishell: ", 2);
 	if (is_absolute(token->data->cmd->path))
 	{
 		if (S_ISDIR(stt.st_mode))
 		{
 			ft_putstr_fd(token->data->cmd->args[0], 2);
-			ft_putstr_fd(": is a directory\n", 2);
+			ft_putstr_fd(": Is a directory\n", 2);
 		}
 		else if (access(token->data->cmd->path, X_OK))
 			perror(token->data->cmd->args[0]);
@@ -115,9 +115,12 @@ unsigned char	command_failed(t_token *token)
 	{
 		ft_putstr_fd("command not found : ", 2);
 		ft_putendl_fd(token->data->cmd->args[0], 2);
-		retval = 127;
+	//	retval = 127;
 	}
-	return (retval);
+	if (is_absolute(token->data->cmd->path) && !access(token->data->cmd->path, F_OK))
+		return (126);
+	return (127);
+//	return (retval);
 }
 
 /** exec_child - executes the cmd in the child process

src/exec/exec_cmd_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_cmd_init.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 12:24:51 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/07/04 11:26:48 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:49:42 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -168,9 +168,9 @@ int	init_cmd(t_tree *node, t_ms *ms)
 		if (!cmd->path)
 			return (perror("minishell"), 1);
 	}
-	if (ms->file_in != STDIN_FILENO)
+	if (ms->file_in != STDIN_FILENO && ms->file_in != -1)
 		node->token->in_fd = ms->file_in;
-	if (ms->file_out != STDOUT_FILENO)
+	if (ms->file_out != STDOUT_FILENO && ms->file_out != -1)
 		node->token->out_fd = ms->file_out;
 	return (0);
 }

src/exec/exec_cmd_utils.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 12:23:44 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/06/22 17:21:50 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 17:04:02 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	dup_handler(t_token *token, t_ms *ms)
 {
 	(void)token;
 	(void)ms;
-	if (ms->file_in != STDIN_FILENO)
+	if (ms->file_in != STDIN_FILENO && ms->file_in != -1)
 		dup2(ms->file_in, STDIN_FILENO);
 	else if (token->in_fd != STDIN_FILENO)
 		dup2(token->in_fd, STDIN_FILENO);
-	if (ms->file_out != STDOUT_FILENO)
+	if (ms->file_out != STDOUT_FILENO && ms->file_out != -1)
 		dup2(ms->file_out, STDOUT_FILENO);
 	else if (token->out_fd != STDOUT_FILENO)
 		dup2(token->out_fd, STDOUT_FILENO);

src/exec/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 18:52:53 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/06/22 18:08:22 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:36:16 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ int	exec_pipe(t_tree *node, t_ms *ms)
 		return (close(pfd[0]), close(pfd[1]), free(pfd), 1);
 	if (node->parent && node->parent->token->type == T_PIPE)
 		node->right->token->out_fd = node->token->out_fd;
-	if (node->left->token->type == T_CMD)
+	if (node->left->token->type == T_CMD || \
+		node->left->token->type == T_PIPE)
 		node->left->token->out_fd = pfd[1];
 	else
 		close(pfd[1]);

src/exec/exec_redir.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 18:52:55 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/06/25 13:47:58 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 17:16:50 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int	open_failed(char *path, t_ms *ms)
 		ft_putstr_fd(path, STDERR_FILENO);
 		ft_putstr_fd(": can't access file.\n", STDERR_FILENO);
 		ms->open_failed = 1;
+		if (ms->file_out == -1)
+			ms->open_failed = -1;
 		return (1);
 	}
 	return (0);

src/exec/exec_tree.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 11:24:47 by tcoeffet          #+#    #+#             */
-/*   Updated: 2025/06/25 14:41:20 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:58:43 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,13 +53,17 @@ int	exec_tree(t_tree *root, t_ms *ms)
 	if (root->token->type == T_HEREDOC)
 		ms->file_in = root->token->data->rd->heredoc->fd[0];
 	exec_tree(root->left, ms);
+	if (root->token->type == T_PIPE)
+		ms->open_failed = 0;
 	exec_tree(root->right, ms);
 	if (ms->open_failed)
-	{
 		ms->retval = 1;
-		return (0);
+	if (root->token->type == T_CMD)
+	{
+		if (!ms->open_failed)
+			exec_cmd(root, ms);
+		else if (root->token->out_fd != STDOUT_FILENO)
+			close(root->token->out_fd);
 	}
-	if (root->token->type == T_CMD && !ms->open_failed)
-		exec_cmd(root, ms);
 	return (0);
 }

src/main.c
@@ -6,7 +6,7 @@
 /*   By: tcoeffet <tcoeffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:30:41 by rcochran          #+#    #+#             */
-/*   Updated: 2025/07/13 14:55:59 by tcoeffet         ###   ########.fr       */
+/*   Updated: 2025/07/13 17:14:48 by tcoeffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,8 @@ int	wait_all(t_ms *ms)
 		i++;
 	}
 	tcsetattr(ms->ms_stdin, TCSADRAIN, ms->term);
+	if (ms->open_failed == -1)
+		return (1);
 	return ((int) ret);
 }
 