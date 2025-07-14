5e9c7157dea6232322cad45c9490d8634094ba7b
Raphael Cochrane <rcochran@f6r7s5.clusters.42paris.fr>
2025-07-14 14:32:54 +0200

    Revert "draft is splitable"
    This reverts commit effbf5b13f90032723f34b3b0b3d121a72578e86.

--- src/expand/expand_cmd_args.c
+++ src/expand/expand_cmd_args.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 10:18:57 by rcochran          #+#    #+#             */
-/*   Updated: 2025/07/10 22:03:50 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/07/14 14:29:58 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,3 +50,86 @@ void	expand_cmd_args(t_cmd **cmd, t_ms *ms)
 	}
 	return ;
 }
+
+void	insert_expanded(char ***args, int *i, char *exp_arg)
+{
+	char	**split_args;
+	int		count;
+	int		j;
+	int		is_quoted;
+
+	is_quoted = check_quote_type((*args)[*i][0]);
+	if (is_quoted)
+	{
+		free((*args)[*i]);
+		(*args)[*i] = exp_arg;
+		return ;
+	}
+	split_args = ft_split(exp_arg, ' ');
+	if (!split_args)
+	{
+		free(exp_arg);
+		perror("minishell");
+		return ;
+	}
+	count = 0;
+	while (split_args[count])
+		count++;
+	free((*args)[*i]);
+	(*args)[*i] = ft_strdup(split_args[0]);
+	if (!(*args)[*i])
+	{
+		free(exp_arg);
+		free_split(split_args);
+		perror("minishell");
+		return ;
+	}
+	j = 1;
+	while (j < count)
+	{
+		insert_arg_at(args, *i + j, split_args[j]);
+		j++;
+	}
+	*i += count - 1;
+	free(exp_arg);
+	free_split(split_args);
+}
+
+void	insert_arg_at(char ***args, int pos, const char *new_arg)
+{
+	int		count;
+	int		j;
+	char	**new_args;
+
+	count = 0;
+	while ((*args)[count])
+		count++;
+	new_args = malloc(sizeof(char *) * (count + 2));
+	if (!new_args)
+	{
+		perror("minishell");
+		return ;
+	}
+	j = 0;
+	while (j < pos)
+	{
+		new_args[j] = (*args)[j];
+		j++;
+	}
+	new_args[pos] = ft_strdup(new_arg);
+	if (!new_args[pos])
+	{
+		free(new_args);
+		perror("minishell");
+		return ;
+	}
+	while (j < count)
+	{
+		new_args[j + 1] = (*args)[j];
+		j++;
+	}
+	new_args[count + 1] = NULL;
+	free(*args);
+	*args = new_args;
+}
+
